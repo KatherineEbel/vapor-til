add4a2085b2592c015320b9a0c6e870c745c5ea7
kathy <kathyebel@me.com>
2018-10-31 14:34:22 -0400

    Add Google login to the website

--- Package.swift
+++ Package.swift
@@ -10,11 +10,18 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.1"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.1")
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
+        .package(url: "https://github.com/vapor-community/Imperial.git", from: "0.7.1")
 
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Leaf", "Authentication"]),
+        .target(name: "App", dependencies: [
+          "FluentPostgreSQL",
+          "Vapor",
+          "Leaf",
+          "Authentication",
+          "Imperial"
+        ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
